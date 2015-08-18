12f69cd6e4a28e00fefb2e3c9c8f54760df15f40
shorty-fr <jerome.demonchaux@gmail.com>
2015-08-18 18:32:34 +0200

    fin des cours

--- magicsquare/src/Controller/Calculator.java
+++ magicsquare/src/Controller/Calculator.java
@@ -1,22 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import View.MainWindow;
-import View.Window;
+import java.util.Arrays;
 
 /**
  *
  * @author Jerome
  */
 public class Calculator {
-    
+
+    Integer matrice[][] = {
+        {1, 15, 14, 4},
+        {12, 6, 7, 9},
+        {8, 10, 11, 5},
+        {13, 3, 2, 16}
+    };
+
     public void init() {
         MainWindow fenetre = new MainWindow();
         fenetre.setVisible(true);
     }
-    
+
+    public Object[][] calcul(int n) {
+        int a = (n - 34) / 4;
+        Integer magicSquare[][] = matrice;
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice.length; j++) {
+                magicSquare[i][j] = matrice[i][j] + a;
+            }
+        }
+        Object[][] x = Arrays.copyOf(magicSquare, magicSquare.length, Integer[][].class);
+        return x;
+    }
+
+    public boolean isPossible(int n) {
+        int a = n - 34;
+        if (0 == a % 4) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- magicsquare/src/View/MainWindow.java
+++ magicsquare/src/View/MainWindow.java
@@ -1,40 +1,88 @@
 package View;
 
+import Controller.Calculator;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableCellRenderer;
 
 /**
  *
  * @author Jerome
  */
 public class MainWindow extends JFrame {
-
+    
     Object matrice[][] = new Object[4][4];
-
+    Calculator ctrl = new Calculator();
     
     public MainWindow() {
         
-        this.setSize(800, 600);
+        this.setSize(800, 700);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        fullfillCarre(matrice);
-        
+        this.setLayout(new GridLayout());
+        genererUI();
+        GenererCarre(ctrl.calcul(38));
         
     }
 
+    /**
+     *
+     */
+    class myhandler implements ActionListener {
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            GenererCarre(ctrl.calcul(Integer.valueOf(e.getActionCommand())));
+            
+        }
+        
+    }
     
-    public void fullfillCarre(Object matrice[][]) {
+    public void genererUI() {
+        JPanel pane = new JPanel();
+        JTextField textfield1 = new JTextField("Entrez un nombre : ");
+        textfield1.setSize(50, 10);
+        JButton bt = new JButton("Valider");
+        myhandler handler = new myhandler();
+        bt.addActionListener(handler);
+        
+        pane.add(textfield1);
+        pane.add(bt);
+        this.add(pane);
+        
+    }
+    
+    public void GenererCarre(Object matrice[][]) {
         Object columns[] = new Object[matrice.length];
         for (int i = 0; i < matrice.length; i++) {
             columns[i] = "";
         }
-
+        
         JTable carre = new JTable(matrice, columns);
-        JScrollPane panel = new JScrollPane(carre);
+        carre.setEnabled(false);
+        carre.setRowHeight(150);
+        //Centrer les valeurs du tableau
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        carre.setDefaultRenderer(Object.class, centerRenderer);
 
-        this.add(panel);
+        // On définit la police et la taille
+        carre.setFont(new Font("Segoe UI", Font.BOLD, 20));
+        JScrollPane panel = new JScrollPane(carre);
+        
+        this.add(panel, BorderLayout.CENTER);
+        
     }
-    
-    
     
 }
