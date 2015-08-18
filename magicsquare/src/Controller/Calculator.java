97a75edb5da3b52752181db2d80436408fc3a0bf
shorty-fr <jerome.demonchaux@gmail.com>
2015-08-18 14:49:53 +0200

    plein de trucs tas vu

---
+++ magicsquare/src/Controller/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.Window;
+
+/**
+ *
+ * @author Jerome
+ */
+public class Calculator {
+    
+    public void init() {
+        Window fenetre = new Window();
+        fenetre.setVisible(true);
+    }
+    
+}
--- magicsquare/src/magicsquare/Magicsquare.java
+++ magicsquare/src/magicsquare/Magicsquare.java
@@ -5,6 +5,8 @@
  */
 package magicsquare;
 
+import Controller.Calculator;
+
 /**
  *
  * @author Jerome
@@ -15,7 +17,8 @@ public class Magicsquare {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Calculator controller = new Calculator();
+        controller.init();
     }
     
 }
